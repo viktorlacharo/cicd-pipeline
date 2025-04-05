ab7d3e987cbb52a8b1ecd3409c94daa989183f06
viktorlacharo <viktorgym1999@gmail.com>
2025-04-05 13:13:20 +0200

    Passing Test

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -4,5 +4,5 @@ import Page from '../src/app/page'
  
 test('Page', () => {
   render(<Page />)
-  expect(screen.getByRole('heading', { level: 1, name: 'Fail' })).toBeDefined()
+  expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined()
 })
